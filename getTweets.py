0d5e67684a312c9f2d0ea7fa86257be0e4392a99
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-01-02 20:37:53 +0000

    Checking content for utf-8
    This is retrieving the response headers and testing the content/type variable to see if the content is utf-8.
    If it is, it is assigning a string variable str_content with the decoded utf-8 contents

--- getTweets.py
+++ getTweets.py
@@ -1,4 +1,21 @@
+# get the http client library >>
 import httplib2
 h = httplib2.Http(".cache")
+# assign variables for headers and content
 resp, content = h.request("http://twitter.com/", "GET")
+# print response headers
 print (resp)
+print ("--------------------")
+# extract content type from header >>
+content_type = {resp['content-type']}
+# assign standard response for utf-8 to test against >>
+test_type = {'text/html;charset=utf-8'}
+# 
+if content_type == test_type :
+    print ("Content type = utf-8")
+    str_content = content.decode('utf-8')
+    print ("contents decoded from utf-8 as...")
+    print (str_content)
+else:
+    print ("failed with message:")
+    print (content_type)
