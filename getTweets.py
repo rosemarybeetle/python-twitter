d393918a38ce1a30a1b61e3dfe86feda0ced9e70
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-01-06 23:41:51 +0000

    Created search as a DEFINED function
    added search_tweets(term,t_type,count)
    This is a defined function that can handle usernames and hashtags without the user having to know the twitter unicode for these.
    Instead, it users the t_type parameter to check ofr usernames or hashtags and add %40 or %23 as required. anything else just searches for the term.
    
    Count is to control the Twitter API parameter '&count=...'

--- getTweets.py
+++ getTweets.py
@@ -1,4 +1,3 @@
-# import httplib2 # not sure this is needed in this. Was in earlier version. 
 import requests
 import requests_oauthlib
 from requests_oauthlib import OAuth1
@@ -15,7 +14,7 @@ print ("PMR_consumer_secret = "+PMRkeys.PMR_consumer_secret)
 print ("PMR_access_token = " + PMRkeys.PMR_access_token)
 print ("PMR_access_secret = "+PMRkeys.PMR_access_secret)
 # -------------- end test ---------------
-
+'''
 # set search url for twitter
 # based on python library 'requests' - documentation here: http://docs.python-requests.org/en/latest/user/authentication/
 search_url = 'https://api.twitter.com/1.1/search/tweets.json?q=%40museweb'
@@ -29,3 +28,33 @@ print ('auth_response.json()')
 auth_json = auth_response.json()
 print (auth_json)
 print ('---------------')
+'''
+
+# ------------- search twitter as a function ---------------
+def search_tweets (term,t_type,count) : # params - term=what to search for. type is how to search. Count = number of tweets (max 100)
+    search_url_root='https://api.twitter.com/1.1/search/tweets.json?q='
+    if t_type=='username':
+        print ('searching twitter API for term: @'+term)
+        term='%40'+term # add unicode for @ sign (%40) if a username search term
+    else:
+        if t_type=='hashtag':
+            print ('searching twitter API for term: #'+term)
+            term='%23'+term # add unicode for # sign (%40) if a hashtag search term
+        else:
+            print ('searching for term: '+term) # or just search!
+    search_url=str(search_url_root+term+'&count='+count)
+    
+    print ('---------------------------')
+    print ()
+    auth = OAuth1(PMRkeys.PMR_consumer_key, PMRkeys.PMR_consumer_secret,PMRkeys.PMR_access_token,PMRkeys.PMR_access_secret )
+    auth_response=requests.get(search_url, auth=auth)
+    print ('auth_response.text')
+    print (auth_response.text)
+    print ('---------------')
+    # get as json. See documentation here: http://docs.python-requests.org/en/latest/user/quickstart/
+    print ('auth_response.json()')
+    auth_json = auth_response.json()
+    print (auth_json)
+    print ('---------------')
+# ------------- end search twitter -------------------------
+search_tweets('museweb','username','2') 
