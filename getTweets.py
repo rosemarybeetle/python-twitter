b548b796b62eeec272e5a17118c4a21ae837560d
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-01-06 21:07:34 +0000

    total revamp

--- getTweets.py
+++ getTweets.py
@@ -1,39 +1,28 @@
 import httplib2
+import requests
+import requests_oauthlib
+from requests_oauthlib import OAuth1
+from requests_oauthlib import OAuth1Session
 import PMRkeys
+import json
+
+
+# -------------- test output ------------
+print ('test print keys for checking correct import')
 print ("PMR_consumer_key = "+PMRkeys.PMR_consumer_key)
 print ("PMR_consumer_secret = "+PMRkeys.PMR_consumer_secret)
 print ("PMR_access_token = " + PMRkeys.PMR_access_token)
 print ("PMR_access_secret = "+PMRkeys.PMR_access_secret)
-h = httplib2.Http(".cache")
-# assign variables for headers and content
-resp, content = h.request("http://twitter.com/", "GET")
-# print response headers
-print ("--------------------")
-print ("--------response headers------------")
-print ()
-print (resp)
-print ()
-print ("--------end response headers--------")
-print ("--------------------")
-print ()
-# extract content type from header >>
-content_type = {resp['content-type']}
-# assign standard response for utf-8 to test against >>
-test_type = {'text/html;charset=utf-8'}
-# 
-if content_type == test_type :
-    print ("Content type = utf-8")
-    str_content = content.decode('utf-8')
-    print ("contents decoded from utf-8 as...")
-    print ("--------------------")
-    print ("--------content------------")
-    print ()
-    print (str_content)
-    print ()
-    print ("--------end content--------")
-    print ("--------------------")
-    print ()
-else:
-    print ("failed with message:")
-    print (content_type)
+# -------------- end test ---------------
 
+# set search url for twitter
+search_url = 'https://api.twitter.com/1.1/search/tweets.json?q=%40museweb'
+auth = OAuth1(PMRkeys.PMR_consumer_key, PMRkeys.PMR_consumer_secret,PMRkeys.PMR_access_token,PMRkeys.PMR_access_secret )
+auth_response=requests.get(search_url, auth=auth)
+print ('auth_response.text')
+print (auth_response.text)
+print ('---------------')
+print ('auth_response.json()')
+auth_json = auth_response.json()
+print (auth_json)
+print ('---------------')
