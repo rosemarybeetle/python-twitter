9c83ebf7023ee02f9097f5818eb24b8e19d2ae32
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-01-09 23:26:27 +0000

    Code tidy up
    sweet!
    in line 41, the search function call is using fixed parameters. 
    If you change them, the function copes, but eventually this call will be made from remote admin settings when running on a Pi

--- getTweets.py
+++ getTweets.py
@@ -2,21 +2,11 @@ import requests
 import requests_oauthlib
 from requests_oauthlib import OAuth1
 from requests_oauthlib import OAuth1Session
-
-import PMRkeys #PMRkeys is a separate .py file with the Twitter Application Oauth credentials listed (not here for obvious reasons!) 
+import PMRkeys #PMRkeys is a separate local .py file with the Twitter Application Oauth credentials listed (not here for obvious reasons!) 
 import json # will be needed to handle json
-
-
-# -------------- test output of twitter authorisation codes from external .py file ------------
-print ('test print keys for checking correct import')
-print ("PMR_consumer_key = "+PMRkeys.PMR_consumer_key)
-print ("PMR_consumer_secret = "+PMRkeys.PMR_consumer_secret)
-print ("PMR_access_token = " + PMRkeys.PMR_access_token)
-print ("PMR_access_secret = "+PMRkeys.PMR_access_secret)
-# -------------- end test ---------------
-
 # ------------- search twitter as a function ---------------
-def search_tweets (term,t_type,count) : # params - term=what to search for. type is how to search. Count = number of tweets (max 100)
+def search_tweets (term,t_type,count) : # params: term= 'what to search for' type = 'how to search' Count = 'number of tweets' (max 100)    search_url_root='https://api.twitter.com/1.1/search/tweets.json?q='
+    # check what type the search term is
     search_url_root='https://api.twitter.com/1.1/search/tweets.json?q='
     if t_type=='username':
         print ('searching twitter API for term: @'+term)
@@ -28,30 +18,24 @@ def search_tweets (term,t_type,count) : # params - term=what to search for. type
         else:
             print ('searching for term: '+term) # or just search!
     search_url=str(search_url_root+term+'&count='+count)
-    
     print ('---------------------------')
     print ()
     auth = OAuth1(PMRkeys.PMR_consumer_key, PMRkeys.PMR_consumer_secret,PMRkeys.PMR_access_token,PMRkeys.PMR_access_secret )
     auth_response=requests.get(search_url, auth=auth)
-    print ('auth_response.text')
-    #print (auth_response.text)
+    # print ('auth_response.text') - uncomment to check the text is returning as expected
+    # print (auth_response.text) - uncomment to check the text is returning as expected
     j = (auth_response.text)
     js = json.loads(j)
     c = int(count)
     x=0
     while (x<c):
-        print ('while loop - x = '+str(x))
         ourResult = js['statuses'][x]['text']
-        print ('---------------')
-        #print ('print js: ')
-        #print (js)
+        # print ('print js: ')
+        # print (js)
         print ('---------------')
         print ('tweet '+str(x+1)+' of '+str(c)+' = ')
         print (ourResult)
         x=x+1
-    
-    
-    
-    
+    print ('---------------')  
 # ------------- end search twitter -------------------------
-search_tweets('museweb','username','5') 
+search_tweets('museweb','username','15') 
