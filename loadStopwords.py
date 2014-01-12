492517b7873a294b4ee44d462d666edf278e53ef
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-01-12 13:25:08 +0000

    Load stopwords to a Python list and count entities

--- loadStopwords.py
+++ loadStopwords.py
@@ -1,24 +1,18 @@
 import requests
-import json
-import csv
 
 stopWords = []
 Ws= requests.get('https://docs.google.com/spreadsheet/pub?key=0AgTXh43j7oFVdFByYk41am9jRnRkeU9LWnhjZFJTOEE&output=csv')
 yy= Ws.text
-print (yy)
-print ('--------')
+stopwords = yy.splitlines()
 
-jj = csv.reader(yy)
-x=0
-for el in jj:
-    if el != []:
-        x+=1
-        print (el)
-'''for line in yy:
-    if line !="\n":
-        x=""
-        x=x+line
-    else:
-        print(x)
-        x=""
-'''
+print ('yy --------')
+print (yy)
+print ('zz --------')
+print (stopwords)
+print ('--------')
+swCount=0
+for count in stopwords:
+    swCount+=1
+
+print ('count -----')
+print (swCount)
