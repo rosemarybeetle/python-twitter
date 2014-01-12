e3139d5e6bbe9d46389d93c6ebd4008811a39df3
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-01-12 13:56:32 +0000

    Retrieve any google spreadsheet as data
    Made the retrieval standard for any valid Google spreadsheet URL to a published sheet (CSV)

--- loadStopwords.py
+++ loadStopwords.py
@@ -1,18 +1,25 @@
 import requests
+adminURL='https://docs.google.com/spreadsheet/pub?key=0AgTXh43j7oFVdGp1NmxJVXVHcGhIel9CNUxJUk8yYXc&output=csv'
+def retrieveArray (url):
+    stopWords = []
+    Ws= requests.get(url)
+    yy= Ws.text
+    stopwords = yy.splitlines()
 
-stopWords = []
-Ws= requests.get('https://docs.google.com/spreadsheet/pub?key=0AgTXh43j7oFVdFByYk41am9jRnRkeU9LWnhjZFJTOEE&output=csv')
-yy= Ws.text
-stopwords = yy.splitlines()
+    print ('stopwords ------------')
+    print (stopwords)
+    print ('--------')
 
-print ('full list returned --------')
-print (yy)
-print ('stopwords --------')
-print (stopwords)
-print ('--------')
-swCount=0
-for count in stopwords:
-    swCount+=1
-
-print ('count -----')
-print (swCount)
+    print ('full list returned raw with line breaks --------')
+    print (yy)
+    print ('stopwords --------')
+    print (stopwords)
+    print ('--------')
+    swCount=0
+    for count in stopwords:
+        swCount+=1
+    print ('count  -----')
+    print ('count = '+str(swCount))
+    # end retrieveArray
+    
+retrieveArray(adminURL)
